290bd9969350c2de778e95d9e9d06b826e206cc0
siddharthkhincha <siddharth.khincha@yahoo.in>
2021-10-18 13:00:51 +0530

    Increased Readability

--- image_downloader.py
+++ image_downloader.py
@@ -1,17 +1,29 @@
+#This is a utility to download the data by scraping images off Google Searches
+
+#To maintain quality of downloaded images, the original source of the image is tracked and downloaded.
+
 from selenium import webdriver
 import urllib.request
 
+def download_data(url,num_images=100,location_to_save=''):
+	web = webdriver.Chrome()
+	 					
+	web.get(url)
+	xpath_p1='//*[@id="islrg"]/div[1]/div['
+	xpath_p2=']/a[1]/div[1]/img'
+	for i in range(1,num_images):
+		try:
+			src = web.find_element_by_xpath(xpath_p1+str(i)+xpath_p2).get_attribute("src")
+			#print(src)
+			urllib.request.urlretrieve(
+	    		src, location_to_save+(str(i))+'.jpg')
+		except:
+			print('Error at number: ',i,'. Continuing with the other images.')
 
-web = webdriver.Chrome()
-url = 'https://www.google.com/search?q=rowing+machine+gym&tbm=isch&ved=2ahUKEwj0nfiBypnyAhVNk0sFHYCBDwwQ2-cCegQIABAA&oq=rowing+machine+gym&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQCBAeMgYIABAIEB4yBggAEAgQHjIGCAAQCBAeMgYIABAIEB4yBggAEAgQHjIGCAAQCBAeOgQIABBDOggIABCABBCxA1CNK1jeL2CaMWgAcAB4AIABdYgBsgOSAQMxLjOYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=prALYfSyNM2mrtoPgIO-YA&bih=728&biw=1390'
-web.get(url)
-xpath_p1='//*[@id="islrg"]/div[1]/div['
-xpath_p2=']/a[1]/div[1]/img'
-for i in range(1,350):
-	try:
-		src = web.find_element_by_xpath(xpath_p1+str(i)+xpath_p2).get_attribute("src")
-		#print(src)
-		urllib.request.urlretrieve(
-    		src, './data/train/rowing/'+(str(i))+'.jpg')
-	except:
-		print(i)
+
+
+if __name__=='main':
+	url = input("Enter the URL of the google search to scrape images off")
+	location_to_save = input("Enter the location to save the images at") 
+	num_images = input("Enter the number of images to download")
+	download_data(url,location_to_save)
